25da9cddba6f10b898cafd5f0456c40e08e59e6e
tehvicke <vikda574@student.liu.se>
2020-02-19 09:03:07 +0100

    import api key from environment variables

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import './App.css'
 
-const API_KEY = process.env.API_KEY
+const API_KEY = process.env.REACT_APP_API_KEY
+
 function App() {
-  useEffect(() => {}, [])
   return (
     <div className='App'>
       <h2>Resturant stuff</h2>
