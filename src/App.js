1ee79e66a210bc017e12cfd868100ee6e05d7f6d
tehvicke <vikda574@student.liu.se>
2020-02-19 09:57:34 +0100

    added basic fetch request in react hook

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import React, { useState, useEffect } from 'react'
 import './App.css'
 
 const API_KEY = process.env.REACT_APP_API_KEY
+const BASE_URL = 'https://developers.zomato.com/api/v2.1'
 
-function App() {
+const populateQuery = async (query, ...rest) => {
+  query = '?q='
+  return query
+}
+
+const App = () => {
+  const [queryString, setQueryString] = useState('')
+  useEffect(() => {
+    populateQuery(queryString)
+    fetch(`${BASE_URL}/search`, {
+      headers: {
+        'user-key': API_KEY
+      }
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+      .catch(err => console.log(err))
+  }, [])
   return (
     <div className='App'>
       <h2>Resturant stuff</h2>
